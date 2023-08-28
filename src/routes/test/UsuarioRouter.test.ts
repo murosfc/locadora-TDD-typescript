01f58fbbae89fa9e174ab3db2ad553f0936ea24f
Felipe Muros <84643909+murosfc@users.noreply.github.com>
2023-08-27 21:33:18 -0300

    Usuario Router teste atualizado para async

--- src/routes/test/UsuarioRouter.test.ts
+++ src/routes/test/UsuarioRouter.test.ts
@@ -14,7 +14,7 @@ afterAll(async () => {
 });
 
 describe("Teste de integração do UsuarioRouter", () => {
-    
+
     it("POST /usuarios/add", async () => {
         const novoUsuario = {
             nome: "Felipe Muros",
@@ -22,43 +22,78 @@ describe("Teste de integração do UsuarioRouter", () => {
             senha: "123456",
             cpf: "12345678901",
         };
-        const response = await request(server).post("/usuarios/add").send(novoUsuario);
-        expect(response.status).toBe(201);
+        try {
+            const response = await request(server).post("/usuarios/add").send(novoUsuario);
+            expect(response.status).toBe(201);
+        }
+        catch (err) {
+            console.log(err);
+        }
     });
 
     it("PUT /usuarios/update/:id", async () => {
         const usuarioAtualizado = {
             nome: "Felipe Muros",
             email: "felipe@gmail.com",
-            senha: "123456789",  
+            senha: "123456789",
             cpf: "12345678901",
-        };  
-        const response = await request(server).put("/usuarios/update/1").send(usuarioAtualizado);
-        expect(response.status).toBe(200);
+        };
+        try {
+            const response = await request(server).put("/usuarios/update/1").send(usuarioAtualizado);
+            expect(response.status).toBe(200);
+        }
+        catch (err) {
+            console.log(err);
+        }
     });
 
     it("GET /usuarios/", async () => {
-        const response = await request(server).get("/usuarios/");
-        expect(response.status).toBe(200);
+        try {
+            const response = await request(server).get("/usuarios/");
+            expect(response.status).toBe(200);
+        }
+        catch (err) {
+            console.log(err);
+        }
     });
 
     it("GET /usuarios/:id", async () => {
-        const response = await request(server).get("/usuarios/1");
-        expect(response.status).toBe(200);
-    }); 
+        try {
+            const response = await request(server).get("/usuarios/1");
+            expect(response.status).toBe(200);
+        }
+        catch (err) {
+            console.log(err);
+        }
+    });
 
     it("GET /usuarios/email/:email", async () => {
-        const response = await request(server).get("/usuarios/email/felipe@gmail.com");
-        expect(response.status).toBe(200);
+        try {
+            const response = await request(server).get("/usuarios/email/felipe@gmail.com");
+            expect(response.status).toBe(200);
+        }
+        catch (err) {
+            console.log(err);
+        }
     });
-    
+
     it("GET /usuarios/cpf/:cpf", async () => {
-        const response = await request(server).get("/usuarios/cpf/12345678901");
-        expect(response.status).toBe(200);
+        try {
+            const response = await request(server).get("/usuarios/cpf/12345678901");
+            expect(response.status).toBe(200);
+        }
+        catch (err) {
+            console.log(err);
+        }
     });
 
     it("DELETE /usuarios/delete/:id", async () => {
-        const response = await request(server).delete("/usuarios/delete/1");
-        expect(response.status).toBe(200);
+        try {
+            const response = await request(server).delete("/usuarios/delete/1");
+            expect(response.status).toBe(200);
+        }
+        catch (err) {
+            console.log(err);
+        }
     });
 });
