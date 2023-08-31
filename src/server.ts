82322d901cb14e7d87249e561c7d9e3fad4e0444
Felipe Muros <84643909+murosfc@users.noreply.github.com>
2023-08-30 21:42:02 -0300

    adicionado proxy

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,7 @@ import loadTestDatabase from './';
 require('dotenv').config();
 
 const app: Express = express();
-const port = process.env.PORT || 3000; 
+const port = process.env.PORT || 8080; 
 loadTestDatabase();
 
 // Middleware to parse incoming JSON data
